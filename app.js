78f583eff951b979967370897ed9e0f9430c78fa
drabikowy <drabikowy.kod@gmail.com>
2017-01-30 20:52:42 +0100

    fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let timelineStart = 10;
-let timelineEnd = 16;
+let timelineEnd = 19;
 let timelineHours = timelineEnd-timelineStart;
 let minutePercent = 100/(timelineHours*60);
 
 
 class Timetable {
-   constructor(from, to, events) {
-      this.start = 10;
-      this.end = 16;
+   constructor(start, end, events) {
+      this.start = start;
+      this.end = end;
       this.hours = this.end-this.start;
       this.minutePercent = 100/(this.hours*60);
       this.events = events;
@@ -25,23 +25,30 @@ class Timetable {
       let row = document.createElement('div');
       row.classList.add("timetable__row");
       // dayTime
-      let day = document.createElement('div');
-      day.classList.add("timetable__timeline");
+      let timeline = document.createElement('div');
+      timeline.classList.add("timetable__timeline");
+
 
       // hours:
-      for (let i=0; i<this.hours; i++) {
+      for (let i=this.start; i<this.end; i++) {
          let hour = document.createElement('div');
+         let label = document.createElement('div');
+         label.classList.add('timeline__hours-label');
+         label.innerText = `${i}:00`;
          hour.classList.add('timeline__hours');
+
+         hour.appendChild(label);
 
          for (let y=0; y<4; y++){
             let quater = document.createElement('div');
             quater.classList.add("timeline__quater");
             hour.appendChild(quater);
          }
-         day.appendChild(hour);
+
+         timeline.appendChild(hour);
       }
 
-      row.appendChild(day);
+      row.appendChild(timeline);
       timetable.appendChild(row);
 
    }
@@ -82,6 +89,7 @@ class Event {
 
 }
 
-let timetable = new Timetable(10,16);
+let timetable = new Timetable(timelineStart,timelineEnd);
 let event = new Event(12.5,14, "Moje zdarzenie","Tomasz",["Aga","Kazek"]);
 let event2 = new Event(14.25,15, "Moje zdarzenie 2","Edward",["Kazek","Tomek"]);
+let event3 = new Event(10.5,11, "Moje zdarzenie 0","Franek",["Edek","Tomek"]);
